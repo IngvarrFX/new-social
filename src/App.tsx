d46f83ba5a39504a697292812c48c4eb1950b2cd
IngvarrFX <nurov.mj@gmail.com>
2022-06-06 17:54:41 +0300

    replacement presentation components on HOC

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import {Header} from "./components/Header";
 import {NavBar} from "./components/NavBar";
-import {Profile} from "./components/Profile";
 import {News} from "./components/News";
 import {Settings} from "./components/Settings";
-import UsersConnect from './components/UsersPage/UsersContainer';
-import "./App.css";
+import UsersConnect from "./components/UsersPage/UsersContainer";
+import ProfileConnect from "./components/Profile/ProfileContainer";
 import DialogsConnect from "./components/Containers/DialogsContainer/DialogsContainer";
+import "./App.css";
+
 
 function App() {
     return (
@@ -16,8 +17,8 @@ function App() {
             <NavBar/>
             <div className="App-wrapper-content">
                 <Routes>
-                    <Route path="/" element={<Profile/>}/>
-                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/" element={<ProfileConnect/>}/>
+                    <Route path="/profile/*" element={<ProfileConnect/>}/>
                     <Route path="/messages/*" element={<DialogsConnect/>}/>
                     <Route path="users" element={<UsersConnect/>}/>
                     <Route path="/music" element={<h1>Music</h1>}/>
