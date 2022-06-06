e832f19bde3085056d00f0d42327488889939efd
IngvarrFX <nurov.mj@gmail.com>
2022-06-06 17:52:51 +0300

    refactor names action creators

--- src/redux/reducers/usersReducer/actions.ts
+++ src/redux/reducers/usersReducer/actions.ts
@@ -15,37 +15,37 @@ export const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 export const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 export const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
-export const followAC = (id: number): FollowACType => {
+export const follow = (id: number): FollowACType => {
     return {
         type: FOLLOW, payload: {id}
     }
 };
 
-export const unFollowAC = (id: number): UnFollowACType => {
+export const unFollow = (id: number): UnFollowACType => {
     return {
         type: UNFOLLOW, payload: {id},
     }
 };
 
-export const setUsersAC = (users: UserType[]): SetUsersACType => {
+export const setUsers = (users: UserType[]): SetUsersACType => {
     return {
         type: SET_USERS, payload: {users},
     }
 };
 
-export const setTotalCountAC = (count: number): SetTotalUsersCountACType => {
+export const setTotalCount = (count: number): SetTotalUsersCountACType => {
     return {
         type: SET_TOTAL_USERS_COUNT, payload: {count},
     }
 };
 
-export const setCurrentPageAC = (pageNumber: number): SetCurrentPageACType => {
+export const setCurrentPage = (pageNumber: number): SetCurrentPageACType => {
     return {
         type: SET_CURRENT_PAGE, payload: {pageNumber},
     }
 };
 
-export const toggleIsFetchingAC = (isFetching: boolean): ToggleIsFetchingACType => {
+export const toggleIsFetching = (isFetching: boolean): ToggleIsFetchingACType => {
     return {
         type: TOGGLE_IS_FETCHING, payload: {isFetching},
     }
