7107510419d548f000b7aabbf320954c3579fe18
IngvarrFX <nurov.mj@gmail.com>
2022-06-11 17:33:21 +0300

    added types

--- src/redux/reducers/usersReducer/types.ts
+++ src/redux/reducers/usersReducer/types.ts
@@ -1,4 +1,12 @@
-import {FOLLOW, SET_CURRENT_PAGE, SET_TOTAL_USERS_COUNT, SET_USERS, TOGGLE_IS_FETCHING, UNFOLLOW} from "./actions";
+import {
+    FOLLOW,
+    SET_CURRENT_PAGE,
+    SET_TOTAL_USERS_COUNT,
+    SET_USERS,
+    TOGGLE_FOLLOWING_PROGRESS,
+    TOGGLE_IS_FETCHING,
+    UNFOLLOW
+} from "./actions";
 
 
 export type UsersActionsType =
@@ -8,6 +16,7 @@ export type UsersActionsType =
     | SetTotalUsersCountACType
     | SetCurrentPageACType
     | ToggleIsFetchingACType
+    | SetToggleFollowingProgressACType
 
 export type FollowACType = {
     type: typeof FOLLOW
@@ -51,12 +60,21 @@ export type ToggleIsFetchingACType = {
     }
 };
 
+export type SetToggleFollowingProgressACType = {
+    type: typeof TOGGLE_FOLLOWING_PROGRESS
+    payload: {
+        isFetching: boolean
+        userId: number
+    }
+};
+
 export type UsersStateType = {
     users: UserType[] | []
     totalUsersCount: number
     currentPage: number
     pageSize: number
     isFetching: boolean
+    toggleFollowingProgress: number[]
 }
 
 export type UserType = {
