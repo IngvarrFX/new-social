c570526c723a578facfb83bb1554a7d0d7ed5094
IngvarrFX <nurov.mj@gmail.com>
2022-06-11 17:32:57 +0300

    refactor request api

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -3,16 +3,16 @@ import {connect, ConnectedProps} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {UserProfileType} from "../../redux/store";
 import {Profile} from "./Profile";
-import {AxiosResponse, default as axios} from "axios";
 import {setUserProfile} from "../../redux/reducers/profileReducer/actions";
+import {profileAPI} from "../../api/api";
 
 type MyState = {}
 
 export class ProfileContainer extends React.Component<PropsFromRedux, MyState> {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${2}`).then((res: AxiosResponse) => {
-            this.props.setUserProfile(res.data);
+        profileAPI.getUserProfile(2).then((userProfileData) => {
+            this.props.setUserProfile(userProfileData);
         });
     }
 
