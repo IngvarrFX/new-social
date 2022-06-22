f8afd9a2729cbe02ba8579c16b857c7e55833b6b
IngvarrFX <nurov.mj@gmail.com>
2022-06-22 15:22:40 +0300

    add import thunks

--- src/redux/reducers/authReducer/index.ts
+++ src/redux/reducers/authReducer/index.ts
@@ -1,3 +1,4 @@
 export {authReducer} from "./authReducer";
 export {setUserData, SET_USER_DATA} from "./actions";
 export type {AuthUserDataType, SetUserDataType, AuthActionsType} from "./types";
+export * as Thunks from "./thunks";
