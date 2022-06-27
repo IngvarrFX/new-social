254fa721773bc4bed63d811816a6bdb121fc863f
IngvarrFX <nurov.mj@gmail.com>
2022-06-27 13:20:08 +0300

    add props profileData and types

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "./Profile.module.css";
 import {ProfileInfo} from "./ProfileInfo";
 import MyPostsConnect from "../MyPostsContainer/MyPostsContainer";
+import {UserProfileType} from "../../redux/types";
 
-type ProfilePropsType = {}
+type ProfilePropsType = {
+    profileData: UserProfileType | null
+}
 
 export const Profile = (props: ProfilePropsType) => {
+    const {profileData} = props;
     return (<div>
-            <ProfileInfo/>
+            <ProfileInfo profileData={profileData}/>
             <div className={styles.PostsBlock}>
                 <MyPostsConnect/>
             </div>
