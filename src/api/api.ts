f172bc0606e4e4b17236e0e45ea78f85d5e32d68
IngvarrFX <nurov.mj@gmail.com>
2022-07-05 13:05:36 +0300

    add env and replace api key

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,15 +2,14 @@ import axios, {AxiosResponse} from "axios";
 import {AuthMeType, FollowingType, UsersResponseType} from "./types";
 import {UserProfileType} from "../redux/types";
 
-
 const api = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
     withCredentials: true,
     headers: {
-        "API-KEY": "1690be4a-0b2f-42a4-9e71-302df103dbfe",
+        // @ts-ignore
+        'api-key': process.env.REACT_APP_API_KEY,
     }
 });
-
 
 export const userAPI = {
     getUsers(currentPage: number, pageSize: number): Promise<UsersResponseType> {
@@ -35,6 +34,12 @@ export const profileAPI = {
     getUserProfile(userId: number): Promise<UserProfileType> {
         return api.get(`profile/${userId}`).then((res: AxiosResponse) => res.data)
     },
+    getProfileStatus(userId: number) {
+        return api.get(`profile/status/${userId}`).then((res: AxiosResponse) => res.data)
+    },
+    updateProfileStatus(status: string){
+        return api.put(`profile/status`,{status}).then((res: AxiosResponse) => res.data)
+    }
 }
 
 
