090a542773366d534a1fe89f48bc25dec2d0cb0f
IngvarrFX <nurov.mj@gmail.com>
2022-07-05 13:11:31 +0300

    created thunk and thunk creator for update profile status

--- src/redux/reducers/profileReducer/thunks.ts
+++ src/redux/reducers/profileReducer/thunks.ts
@@ -2,11 +2,20 @@ import {profileAPI} from "../../../api/api";
 import {ThunkAction} from "redux-thunk";
 import {AppStateType} from "../../redux-store";
 import {AnyAction} from "redux";
-import {setUserProfile} from "./actions";
+import {setProfileStatus, setUserProfile} from "./actions";
 
 
 export const getUserProfileTC = (userId: number): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
     profileAPI.getUserProfile(Number(userId)).then((userProfileData) => {
         dispatch(setUserProfile(userProfileData));
+        profileAPI.getProfileStatus(userProfileData.userId).then((status) => {
+            dispatch(setProfileStatus(status))
+        })
     });
+}
+
+export const updateProfileStatusTC = (status: string): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+    profileAPI.updateProfileStatus(status).then((res) => {
+        dispatch(setProfileStatus(status))
+    })
 }
