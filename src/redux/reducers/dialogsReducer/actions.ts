d9d4a1795badb96a47af9556992fb0fc32242867
IngvarrFX <nurov.mj@gmail.com>
2022-07-08 12:23:47 +0300

    added argument message

--- src/redux/reducers/dialogsReducer/actions.ts
+++ src/redux/reducers/dialogsReducer/actions.ts
@@ -1,19 +1,19 @@
-import {AddMessageACType, NewMessageTextACType} from "./types";
+import {AddMessageACType} from "./types";
 
 export const ADD_MESSAGE = "ADD_MESSAGE";
 export const NEW_MESSAGE_TEXT = "NEW_MESSAGE_TEXT";
 
 
-export const addMessage = (id: string): AddMessageACType => {
+export const addMessage = (id: string, message: string): AddMessageACType => {
     return {
-        type: ADD_MESSAGE, payload: {id}
+        type: ADD_MESSAGE, payload: {id, message}
     }
 };
 
-
+/*
 
 export const newMessageText = (messageBody: string): NewMessageTextACType => {
     return {
         type: NEW_MESSAGE_TEXT, payload: {value: messageBody},
     }
-};
+};*/
