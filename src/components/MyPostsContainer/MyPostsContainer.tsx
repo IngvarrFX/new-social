5179d5c4c987218c34192474789a848feb4afec6
IngvarrFX <nurov.mj@gmail.com>
2022-07-08 12:19:38 +0300

    remove unusable props and types and added argument for addNewPostHandle

--- src/components/MyPostsContainer/MyPostsContainer.tsx
+++ src/components/MyPostsContainer/MyPostsContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {MyPosts} from "../MyPosts";
 import {connect} from "react-redux";
 import {AppStateType, DispatchType} from "../../redux/redux-store";
-import {addPostAC, newPostTextAC} from "../../redux/reducers/profileReducer/actions";
+import {addPostAC} from "../../redux/reducers/profileReducer/actions";
 import {compose} from "redux";
 import {withAuthRedirect} from "../../customHOCs/withAuthRedirect";
 import {PostType} from "../../types";
@@ -13,34 +13,27 @@ type MapStateToPropsType = {
 }
 
 type MapDispatchToPropsType = {
-    addPostCallback: () => void
-    newPostTextCallback: (value: string) => void
+    addPostCallback: (newPost: string) => void
 }
 
 type MyPostsPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 const MyPostsContainer = (props: MyPostsPropsType) => {
-    const {posts, value, addPostCallback, newPostTextCallback} = props;
+    const {posts, addPostCallback} = props;
 
-    const addNewPostHandle = () => {
-        addPostCallback();
+    const addNewPostHandle = (newPost: string) => {
+        addPostCallback(newPost);
     };
 
-    const newPostTextHandle = (text: string) => {
-        newPostTextCallback(text);
-    }
-
-    return <MyPosts posts={posts} addPost={addNewPostHandle} newPostText={newPostTextHandle} value={value}/>
+    return <MyPosts posts={posts} addPost={addNewPostHandle}/>
 };
 
 const mapStateToProps = (state: AppStateType) => ({
     posts: state.profilePage.posts,
-    value: state.profilePage.newPostText,
 })
 
 const mapDispatchToProps = (dispatch: DispatchType) => ({
-    addPostCallback: () => dispatch(addPostAC()),
-    newPostTextCallback: (value: string) => dispatch(newPostTextAC(value))
+    addPostCallback: (newPost: string) => dispatch(addPostAC(newPost)),
 })
 
 export default compose<React.ComponentType>(withAuthRedirect, connect(mapStateToProps, mapDispatchToProps))(MyPostsContainer)
