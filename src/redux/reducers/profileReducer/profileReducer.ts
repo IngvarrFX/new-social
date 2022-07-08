5e3036e5204db1f5efc84f11aec76d83ec7f9f9c
IngvarrFX <nurov.mj@gmail.com>
2022-07-08 12:26:03 +0300

    refactored reducer (removed case and property from initialState)

--- src/redux/reducers/profileReducer/profileReducer.ts
+++ src/redux/reducers/profileReducer/profileReducer.ts
@@ -1,14 +1,13 @@
 import {profilePageType} from "../../types";
 import {v1} from "uuid";
 import {ProfileActionsType} from "./types";
-import {ADD_POST, NEW_POST_MESSAGE, SET_PROFILE_STATUS, SET_USER_PROFILE} from "./actions";
+import {ADD_POST, SET_PROFILE_STATUS, SET_USER_PROFILE} from "./actions";
 
 const initialState: profilePageType = {
     posts: [
         {id: v1(), message: "Hello world!", likes: 3},
         {id: v1(), message: "My first post!", likes: 5},
     ],
-    newPostText: "",
     userProfile: null,
     profileStatus: "",
 };
@@ -18,12 +17,8 @@ export const profileReducer = (state: profilePageType = initialState, action: Pr
         case ADD_POST: {
             return {
                 ...state,
-                posts: [{id: v1(), message: state.newPostText, likes: 0}, ...state.posts],
-                newPostText: ""
+                posts: [{id: v1(), message: action.newPost, likes: 0}, ...state.posts],
             };
-        }
-        case NEW_POST_MESSAGE: {
-            return {...state, newPostText: action.payload.value};
         }
         case SET_USER_PROFILE: {
             return {...state, userProfile: {...action.payload.userProfile}}
