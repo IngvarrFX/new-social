8bfaef985fc6f3722b8eb888e6ca3984087dd51b
IngvarrFX <nurov.mj@gmail.com>
2022-07-08 12:28:56 +0300

    removed unusable handler and added argument message for onClickHandle

--- src/components/Containers/DialogsContainer/DialogsContainer.tsx
+++ src/components/Containers/DialogsContainer/DialogsContainer.tsx
@@ -3,49 +3,41 @@ import {Dialogs} from "../../Dialogs";
 import {connect} from "react-redux";
 import {AppStateType, DispatchType} from "../../../redux/redux-store";
 import {useParams} from "react-router-dom";
-import {addMessage, newMessageText} from "../../../redux/reducers/dialogsReducer/actions";
+import {addMessage} from "../../../redux/reducers/dialogsReducer/actions";
 import {withAuthRedirect} from "../../../customHOCs/withAuthRedirect";
 import {compose} from "redux";
 import {DialogsType} from "../../../types";
 
 type MapStateToPropsType = {
     dialogs: DialogsType[] | undefined
-    value: string
 }
 
 type MapDispatchToPropsType = {
-    addMessageCallback: (id: string) => void
-    newMessageTextCallback: (value: string) => void
+    addMessageCallback: (id: string, message: string) => void
 }
 
 type DialogsPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 export const DialogsContainer = (props: DialogsPropsType) => {
-    const {dialogs, value, newMessageTextCallback, addMessageCallback} = props;
+    const {dialogs, addMessageCallback} = props;
 
     const params = useParams();
 
-    const onClickHandle = () => {
+    const onClickHandle = (message: string) => {
         if (params["*"]) {
-            addMessageCallback(params["*"]);
+            addMessageCallback(params["*"], message);
         }
     };
 
-    const newMessageText = (text: string) => {
-        newMessageTextCallback(text);
-    };
-
-    return <Dialogs dialogs={dialogs} value={value} callBack={onClickHandle} newMessage={newMessageText}/>
+    return <Dialogs dialogs={dialogs} callBack={onClickHandle} />
 };
 
 const mapStateToProps = (state: AppStateType) => ({
     dialogs: state.messagePage.dialogs,
-    value: state.messagePage.newMessage,
 })
 
 const mapDispatchToProps = (dispatch: DispatchType) => ({
-    addMessageCallback: (id: string) => dispatch(addMessage(id)),
-    newMessageTextCallback: (value: string) => dispatch(newMessageText(value))
+    addMessageCallback: (id: string, message: string) => dispatch(addMessage(id, message))
 })
 
 export default compose<React.ComponentType>(withAuthRedirect,connect(mapStateToProps, mapDispatchToProps))(DialogsContainer)
