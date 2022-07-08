0dc88a9f4567ba0534d9a8eaef18b7a730e8bd91
IngvarrFX <nurov.mj@gmail.com>
2022-07-08 12:25:05 +0300

    created thunk for app

---
+++ src/redux/reducers/appReducer/thunks.ts
@@ -0,0 +1,8 @@
+import {ThunkAction} from "redux-thunk";
+import {AppStateType} from "../../redux-store";
+import {AnyAction} from "redux";
+
+
+export const appTC = (): ThunkAction<void, AppStateType, unknown, AnyAction> => (dispatch) => {
+
+}
