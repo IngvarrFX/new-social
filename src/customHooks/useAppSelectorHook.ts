2edcc8e4ed78e3fe52cab49df922652c9ef06e4a
IngvarrFX <nurov.mj@gmail.com>
2022-07-11 12:47:42 +0300

    created useAppSelectorHook

---
+++ src/customHooks/useAppSelectorHook.ts
@@ -0,0 +1,3 @@
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {AppStateType} from "../redux/redux-store";
+export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector;
