2f852be30943458bb1c2491a1b46753eb48e3ab9
IngvarrFX <nurov.mj@gmail.com>
2022-07-11 12:49:32 +0300

    added redirect

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import {LoginForm} from "../Form";
+import {useNavigate} from "react-router-dom";
+import {useAppSelector} from "../../customHooks/useAppSelectorHook";
 
 export const Login = () => {
-    return (
-        <h1>
+    const {isAuth, userId} = useAppSelector(state => state.auth);
+
+    const navigate = useNavigate();
+    if (isAuth) {
+        navigate(`/profile/${userId}`)
+    }
+
+    return (<>
+            <h1>Login</h1>
             <LoginForm/>
-        </h1>
+        </>
     );
 };
